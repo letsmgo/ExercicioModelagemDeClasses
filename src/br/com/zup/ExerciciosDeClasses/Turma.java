26f8bc00bdea34ddfbefc1d714334e59642afaa1
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-26 20:50:12 -0300

    LEVEL UP - Criando Classe turma

---
+++ src/br/com/zup/ExerciciosDeClasses/Turma.java
@@ -0,0 +1,32 @@
+package br.com.zup.ExerciciosDeClasses;
+
+public class Turma {
+    private int qtdAlunos;
+    private String semestre;
+
+    //Método construtor
+    public Turma(int qtdAlunos, String semestre) {
+        this.qtdAlunos = qtdAlunos;
+        this.semestre = semestre;
+    }
+
+    //Getters and Setters
+
+    public int getQtdAlunos() {
+        return qtdAlunos;
+    }
+
+    public void setQtdAlunos(int qtdAlunos) {
+        this.qtdAlunos = qtdAlunos;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+
+}
