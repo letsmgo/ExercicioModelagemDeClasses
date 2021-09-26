a0f2c1ed454938ef80bf7d325d1c6648939b882a
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-26 13:10:52 -0300

    Criando a classe coordenadores com o método do construtor

---
+++ src/br/com/zup/ExerciciosDeClasses/Coordenadores.java
@@ -0,0 +1,17 @@
+package br.com.zup.ExerciciosDeClasses;
+
+public class Coordenadores extends Funcionarios{
+    private String profsSupervisionados;
+
+    //Método construtor
+    public Coordenadores (){
+
+    }
+
+    public Coordenadores(String nome, String cpf, double numRegistro, String orgaoLotacao, double salario, String profsSupervisionados) {
+        super(nome, cpf, numRegistro, orgaoLotacao, salario);
+        this.profsSupervisionados = profsSupervisionados;
+    }
+
+
+}
