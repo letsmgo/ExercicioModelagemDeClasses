7a22baa54c8585873c8bfd1889f54750905e9543
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-26 13:09:12 -0300

    Corrigindo sintaxe e limpando o codigo da classe Professores

--- src/br/com/zup/ExerciciosDeClasses/Professores.java
+++ src/br/com/zup/ExerciciosDeClasses/Professores.java
@@ -1,13 +1,13 @@
 package br.com.zup.ExerciciosDeClasses;
 
-public class Professores extends Funcionarios{
+public class Professores extends Funcionarios {
     private String nivelDeGraduacao;
     private String disciplinaMinistrada;
     private int qtdAlunos;
     private int qtdTurmas;
 
     //Método construtor
-    public Professores(){
+    public Professores() {
 
     }
 
@@ -19,5 +19,37 @@ public class Professores extends Funcionarios{
         this.qtdTurmas = qtdTurmas;
     }
 
+    //Getters and Setters
+    public String getNivelDeGraduacao() {
+        return nivelDeGraduacao;
+    }
+
+    public void setNivelDeGraduacao(String nivelDeGraduacao) {
+        this.nivelDeGraduacao = nivelDeGraduacao;
+    }
+
+    public String getDisciplinaMinistrada() {
+        return disciplinaMinistrada;
+    }
+
+    public void setDisciplinaMinistrada(String disciplinaMinistrada) {
+        this.disciplinaMinistrada = disciplinaMinistrada;
+    }
+
+    public int getQtdAlunos() {
+        return qtdAlunos;
+    }
+
+    public void setQtdAlunos(int qtdAlunos) {
+        this.qtdAlunos = qtdAlunos;
+    }
+
+    public int getQtdTurmas() {
+        return qtdTurmas;
+    }
+
+    public void setQtdTurmas(int qtdTurmas) {
+        this.qtdTurmas = qtdTurmas;
+    }
 
 }
